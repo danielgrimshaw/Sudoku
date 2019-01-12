a392a4150287ce69d27ed124c25eb13c176f062d
Daniel Grimshaw <danielgrimshaw@berkeley.edu>
2019-01-12 12:08:48 -0800

    Write serial version of main

---
+++ main.hs
@@ -0,0 +1,14 @@
+import Sudoku
+import Control.Exception
+import System.Environment
+import Data.Maybe
+
+main :: IO ()
+main = do
+    [f] <- getArgs
+    file <- readFile f
+
+    let puzzles = lines file
+        solutions = map solve puzzles
+
+    print (length (filter isJust solutions))
